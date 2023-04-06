0edc54a89b4798486d31476849b8b51a708e3d75
jooabbas99 <youssef.ahmedabbas99@gmail.com>
2023-04-07 00:09:57 +0200

    LED V0

--- sprint 3/S_ESL1_02_LED sequence V1.0/Code/GccApplication1/GccApplication1/main.c
+++ sprint 3/S_ESL1_02_LED sequence V1.0/Code/GccApplication1/GccApplication1/main.c
@@ -14,21 +14,10 @@
 int main(void)
 {
     /* Replace with your application code */
-	
-	// set pin direction 
-	// DDRA |= (1<<0);
-	//DIO_init(PORT_A,PIN4,OUT);
-	//DIO_init(PORT_B,PIN3,OUT);
-	
-	//LED_init(PORT_A,PIN4);
+
 	AppInit();
     while (1) 
-    {
-		// make pin high 
-		//PORTA |= (1<<0);
-		//DIO_write(PORT_A,PIN4,HIGH);
-		//DIO_write(PORT_B,PIN3,LOW);
-		//LED_on(PORT_A,PIN4);
+	{
 		
 		AppStart();
 	
